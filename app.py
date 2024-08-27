c7c6493033eb055e3fe94516cc80d571820b6a6d
Brian Maxwell Ketaren <86067355+brianketaren14@users.noreply.github.com>
2024-08-27 13:45:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-import pickle
+import tensorflow as tf
 
 st.write(
     """
@@ -11,39 +11,42 @@ st.write(
 
 x1 = st.number_input('Curricular units 2nd sem (approved)')
 x2 = st.number_input('Curricular units 2nd sem (grade)')
-x3 = st.number_input('Curricular units 1st sem (approved)')
-x4 = st.number_input('Curricular units 1st sem (grade)')
+x3 = st.number_input('Curricular units 1st sem (grade)')
+x4 = st.number_input('Curricular units 1st sem (approved)')
 x5 = st.selectbox(
     "Tuition fees up to date",
     (1, 0,),
 )
 
 x6 = st.number_input('Curricular units 2nd sem (evaluations)')
-x7 = st.selectbox(
+
+x7 = st.number_input('Curricular units 1st sem (evaluations)')
+
+x8 = st.selectbox(
     'Scholarship holder',
     (1,0),
 )
-
-x8 = st.number_input('Curricular units 1st sem (evaluations)')
 
 x9 = st.number_input('Age at enrollment')
 
 x10 = st.number_input('Admission grade')
 
 if st.button('predict'):
-    x = np.array([x1, x2, x3, x4, x6, x8, x9, x10]).reshape(-1, 1)
+    x = np.array([x1, x2, x3, x4, x6, x7, x9, x10]).reshape(-1, 1)
     # x5, x8
     scaler = MinMaxScaler()
     x = scaler.fit_transform(x)
     
 
 
-    x_append = [x[0][0], x[1][0], x[2][0], x[3][0], x5, x[4][0], x[5][0], x7, x[6][0], x[7][0]]
-    input_array = np.array(x_append)
+    x_append = np.array([x[0][0], x[1][0], x[2][0], x[3][0], x5, x[4][0], x[5][0], x8, x[6][0], x[7][0]])
+    input_array = np.reshape(x_append, (1,-1))
 
-    model = pickle.load(open('model.sav', 'rb'))
+    model = tf.keras.models.load_model('my_model.h5')
 
-    y = model.predict([input_array])[0]
+    y = model.predict(input_array)
+
+    y = np.argmax(y)
 
     if y == 2:
         st.title("Graduated")
@@ -51,3 +54,4 @@ if st.button('predict'):
         st.title("Enrolled")
     elif y == 0:
         st.title("Dropout")
+    
